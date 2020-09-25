0dd55da7b42e5f17e1dc8ed7db9874c4537d266d
Oluwatobi Akinseye <tobi.akinseye@gmail.com>
2020-09-25 16:21:36 +0100

    first commit

---
+++ WebsiteStatus/Program.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+using Serilog.Events;
+
+namespace WebsiteStatus
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration()
+                              .MinimumLevel.Debug()
+                              .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+                              .Enrich.FromLogContext()
+                              .WriteTo.File("./LogFile.txt")
+                              .WriteTo.Console(LogEventLevel.Information)
+                              .CreateLogger();
+
+            try
+            {
+                Log.Information("Starting up the service");
+                CreateHostBuilder(args).Build().Run();
+                return;
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "There was a problem with the service");
+                return;
+            }
+
+            finally {
+                Log.CloseAndFlush();
+
+            }
+           
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddHostedService<Worker>();
+                    
+                }).UseSerilog();
+    }
+}
---
+++ WebsiteStatus/Worker.cs
@@ -0,0 +1,72 @@
+using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System.Diagnostics;
+using System.Net;
+
+namespace WebsiteStatus
+{
+    public class Worker : BackgroundService
+    {
+        private readonly ILogger<Worker> _logger;
+        private HttpClient client;
+
+        public Worker(ILogger<Worker> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task StartAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation($"Worker started at: {DateTimeOffset.Now}");
+            client = new HttpClient();
+            return base.StartAsync(cancellationToken);
+        }
+
+        public override Task StopAsync(CancellationToken cancellationToken)
+        {
+             client.Dispose();
+            _logger.LogInformation("Worker service stopped.......");
+            return base.StopAsync(cancellationToken);
+        }
+
+
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            var stopwatch = Stopwatch.StartNew();
+            while (!stoppingToken.IsCancellationRequested)
+            { 
+                stopwatch.Start();
+                try
+                {
+                    var result = await client.GetAsync("http://www.gcccng.org");
+                    if (result.StatusCode == System.Net.HttpStatusCode.OK)
+                    {
+                        _logger.LogInformation($"GCCC website is up. Status Code {result.StatusCode}");
+                    }
+                    else
+                    {
+                        _logger.LogInformation($"GCCC website is down. Status Code {result.StatusCode}");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogError(ex.Message, ex);
+                }
+
+                finally {
+                    await Task.Delay(2500, stoppingToken);
+                    stopwatch.Stop();
+                    
+                    var timeTaken = (stopwatch.ElapsedMilliseconds);
+                    _logger.LogInformation($"Process took a total of {timeTaken} milliseconds");
+                    stopwatch.Reset();
+                }                
+            }
+        }
+    }
+}
